2ad34838bd7f617dfe1ae2981919f0961ca17616
Vaibhavi Khamar <khamar.v@northeastern.edu>
2020-01-30 12:16:27 -0500

    testing

--- assignment3/test.js
+++ assignment3/test.js
@@ -18,35 +18,35 @@ describe('models check', function () {
     });
 });
 
-describe('API endpoint GET', function () {
+// describe('API endpoint GET', function () {
 
-    before(function () {
+//     before(function () {
 
-    });
+//     });
 
-    after(function () {
+//     after(function () {
 
-    });
+//     });
 
-    it('should check body type & statuscode', function () {
-        return chai.request(app)
-            .get('/user/self')
-            .then(function (res) {
-                // expect(res).to.have.status(200);
-                // expect(res).to.be.json;
-                expect(res.body).to.be.an('object');
-            });
-    });
-    // GET - Invalid path
-    // it('should return Not Found', function () {
-    //     return chai.request(app)
-    //         .get('/INVALID_PATH')
-    //         .then(function (res) {
-    //             // throw new Error('Path exists!');
-    //             expect(res).to.have.status(404);
-    //         })
-    //         .catch(function (err) {
-    //             expect(err).to.have.status(404);
-    //         });
-    // });
-});
+//     it('should check body type & statuscode', function () {
+//         return chai.request(app)
+//             .get('/user/self')
+//             .then(function (res) {
+//                 // expect(res).to.have.status(200);
+//                 // expect(res).to.be.json;
+//                 expect(res.body).to.be.an('object');
+//             });
+//     });
+//     // GET - Invalid path
+//     // it('should return Not Found', function () {
+//     //     return chai.request(app)
+//     //         .get('/INVALID_PATH')
+//     //         .then(function (res) {
+//     //             // throw new Error('Path exists!');
+//     //             expect(res).to.have.status(404);
+//     //         })
+//     //         .catch(function (err) {
+//     //             expect(err).to.have.status(404);
+//     //         });
+//     // });
+// });
