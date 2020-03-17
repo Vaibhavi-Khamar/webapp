932703fa86ebe3b173e808459cf9d30feae948d4
Vaibhavi Khamar <khamar.v@northeastern.edu>
2020-03-17 12:48:35 -0400

    readme updatee

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/basha
 
 cd /opt/codedeploy-agent/deployment-root/
-mkdir home/ubuntu/webapp
-cd home/ubuntu/webapp
-npm install -g pm2
-npm install
+mkdir /home/ubuntu/webapp
+cd /home/ubuntu/webapp
+sudo npm install -g pm2
+sudo npm install
