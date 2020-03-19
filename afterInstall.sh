5f290f4c070c8ece7e6eee1683b00788d7d1c81b
Vaibhavi Khamar <khamar.v@northeastern.edu>
2020-03-19 19:26:00 -0400

    pm2 issue

--- afterInstall.sh
+++ afterInstall.sh
@@ -4,3 +4,9 @@ mkdir /home/ubuntu/webapp
 cd /home/ubuntu/webapp
 echo "installing npm"
 sudo npm install
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/ubuntu/webapp/AmazonCloudWatch-config.json \
+    -s
