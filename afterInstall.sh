2f92770846a6fd74a94cd00d744b89ad81a11d5b
Vaibhavi Khamar <khamar.v@northeastern.edu>
2020-03-18 21:43:31 -0400

    readme updatee

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "creating dir"
 mkdir /home/ubuntu/webapp
 cd /home/ubuntu/webapp
+echo "installing npm"
 sudo npm install
--- beforeInstall.sh
+++ beforeInstall.sh
@@ -5,3 +5,4 @@ sudo npm install -g pm2
 cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
 echo "removing webapp"
 sudo rm -rf webapp
+echo "removed webapp"
