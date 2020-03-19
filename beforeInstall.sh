c2c76c6b96cb93f865a63d67d4f53fc29445b8b1
Vaibhavi Khamar <khamar.v@northeastern.edu>
2020-03-18 22:51:01 -0400

    readme updatee

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -2,7 +2,6 @@
 sudo apt-get update
 echo "installing pm2"
 sudo npm install -g pm2
-sudo npm rebuild bcrypt --update-binary
 cd /opt/codedeploy-agent/deployment-root/deployment-instructions/
 echo "removing webapp"
 sudo rm -rf webapp
